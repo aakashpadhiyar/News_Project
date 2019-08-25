a1f064d7f1928de10587f1b262ed1c92b17c0586
Aakash Padhiyar <aakashpadhiyar06@gmail.com>
2019-08-25 18:36:39 +0530

    working mode

---
+++ users/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+# Form import
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+#class for admin
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email','username', 'age', 'is_staff', ]
+
+# Register your models here.
+
+admin.site.register(CustomUser, CustomUserAdmin)
