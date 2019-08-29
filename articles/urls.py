65b4703c3150832d914567dae5960eabbc0b97f2
Aakash Padhiyar <aakashpadhiyar06@gmail.com>
2019-08-29 13:28:08 +0530

    newspaper app complate-1

--- articles/urls.py
+++ articles/urls.py
@@ -4,10 +4,12 @@ from .views import (
     ArticleUpdateView,
     ArticleDetailView,
     ArticleDeleteView,
+    ArticleCreateView,
 )
 urlpatterns = [
     path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
-    path('', ArticleListView.as_view(), name='article_list'),
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
+    path('', ArticleListView.as_view(), name='article_list'),    
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from .models import Article
 # Create your views here.
@@ -26,3 +26,10 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_name = 'article_delete.html'
     success_url = reverse_lazy('article_list')
+
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body', 'author',)
